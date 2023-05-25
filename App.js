b627db8ce0b45f2bad2c6a11254a497d79420362
Niklas Meyer <>
2023-05-25 23:32:23 +0200

    aufräumen

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Dashboard, Discover, Live, Glasses } from "./screens";
-import Login from "./screens/auth/Login";
+import {StyleSheet} from "react-native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import React from "react";
-import Welcome from "./screens/auth/Welcome";
-import UserStack from "./navigation/UserStack";
-import AuthStack from "./navigation/authStack";
 import RootNavigation from "./navigation";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-      <RootNavigation />
-  );
+    return (
+        <RootNavigation/>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
