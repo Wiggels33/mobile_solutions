b460a54c0838958da8e515147657e13d0ecd33f0
Niklas Meyer <>
2023-06-18 16:17:00 +0200

    working stage

--- App.js
+++ App.js
@@ -1,22 +1,5 @@
-import {StyleSheet} from "react-native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import React from "react";
 import RootNavigation from "./navigation";
-
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
 export default function App() {
-    return (
-        <RootNavigation/>
-    );
+  return <RootNavigation />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
--- navigation/index.js
+++ navigation/index.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import UserStack from "./UserStack";
 import AuthStack from "./authStack";
-import {useAuthentication} from "../utils/hooks/useAuthentication";
+import { useAuthentication } from "../utils/hooks/useAuthentication";
 
 const RootNavigation = () => {
   const { user } = useAuthentication();
 
+  if (user === undefined) {
+    console.log("USER === NULL");
+  }
 
-  return !user ? <UserStack /> : <AuthStack />;
-}
+  return user ? <UserStack /> : <AuthStack />;
+};
 
 export default RootNavigation;
